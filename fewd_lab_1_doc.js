b83467fe2bbb9eb442fddb2700565b7feabde117
brnborges <brn5haxx@gmail.com>
2020-01-29 04:19:46 +0000

    commit

---
+++ fewd_lab_1_doc.js
@@ -0,0 +1,32 @@
+//Name: Bruno Borges
+//Student ID: 20053
+//Repository link: https://github.com/brnborges/fewd_lab_1
+
+var firstName = 'Bruno';
+var favouriteColor = 'Black';
+
+var favouriteFood = 'Pizza';
+var city = 'Sao Paulo';
+
+// 6. Which value does x have after execution of the following code?
+//A: 'Toe'
+
+//7. Which value does y have after execution of the following code?
+//A: 'Z'
+
+// The console log going to print out the following variables 
+console.log('Name: ' + firstName + "\n" + 'Color: ' + favouriteColor);
+console.log('Food: ' + favouriteFood);
+console.log('City: ' + city + "\n");
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + ". " + one + ", " + two + ", " + three + ", " + four + ", " + five + ", " + six + ", " + seven + " " + endingPhrase);
